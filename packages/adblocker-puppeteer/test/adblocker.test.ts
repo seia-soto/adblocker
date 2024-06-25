072703de91d99b87674d328f4ca50f5a9100b1b5
HoJeong Go <seia@outlook.kr>
2024-06-25 20:31:59 +0900

    test: adblocker-puppeteer e2e testing

--- packages/adblocker-puppeteer/test/adblocker.test.ts
+++ packages/adblocker-puppeteer/test/adblocker.test.ts
@@ -3,6 +3,8 @@ import 'mocha';
 
 import * as puppeteer from 'puppeteer';
 import * as http from 'http';
+import * as path from 'path';
+import { createReadStream, existsSync, statSync } from 'fs';
 
 import {
   fromPuppeteerDetails,
@@ -71,7 +73,7 @@ describe('#stylesInjection', () => {
   it('does not inject styles into original content', async () => {
     const url = `http://localhost:${port}`;
     const stylesInjectionPrefix = '<style';
-    const blocker = await PuppeteerBlocker.parse('###Meebo\\:AdElement\\.Root');
+    const blocker = PuppeteerBlocker.parse('###Meebo\\:AdElement\\.Root');
 
     await blocker.enableBlockingInPage(page);
     await page.goto(url, { waitUntil: 'networkidle2' });
@@ -83,6 +85,106 @@ describe('#stylesInjection', () => {
 
     expect(contentWithoutAds).to.contain(stylesInjectionPrefix);
     expect(content).not.to.contain(stylesInjectionPrefix);
+  });
+
+  after(async () => {
+    await page.close();
+    console.log('Puppeteer page closed.');
+    await browser.close();
+    console.log('Puppeteer browser closed.');
+    server.close(() => {
+      console.log('Test server closed.');
+    });
+  });
+});
+
+describe.only('e2e', () => {
+  let server: http.Server;
+  let port: number;
+  let browser: puppeteer.Browser;
+  let page: puppeteer.Page;
+
+  before(async () => {
+    const basePath = './test/website';
+
+    const assumeContentType = (ext: string) => {
+      switch (ext) {
+        case '.js':
+          return 'text/javascript';
+        case '.css':
+          return 'text/css';
+        case '.html':
+          return 'text/html';
+        default:
+          return 'text/plain';
+      }
+    };
+
+    // Open a simple file-server to serve the testing website
+    server = http
+      .createServer((req, res) => {
+        if (req.method !== 'GET') {
+          res.writeHead(400, { 'Content-Type': 'text/plain' });
+          res.end('This server do not handle requests rather than with a method of GET.');
+          return;
+        }
+
+        if (typeof req.url === 'undefined' || req.url === '/') {
+          req.url = '/index.html';
+        }
+
+        const reqPath = path.join(basePath, req.url);
+
+        if (!existsSync(reqPath) || !statSync(reqPath).isFile()) {
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          res.end('Not found');
+          return;
+        }
+
+        res.writeHead(200, { 'Content-Type': assumeContentType(path.extname(reqPath)) });
+
+        const source = createReadStream(reqPath);
+        source.once('open', () => {
+          source.pipe(res);
+        });
+        source.once('end', () => {
+          res.end();
+        });
+      })
+      .listen(0, () => {
+        const addressInfo = server.address() as AddressInfo;
+        port = addressInfo.port;
+        console.log('Test server listening on port', port);
+      });
+    browser = await puppeteer.launch();
+    console.log('Puppeteer browser launched.');
+    page = await browser.newPage();
+    console.log('Puppeteer page opened.');
+  });
+
+  describe('replace modifier', () => {
+    it('replaces inline script', async () => {
+      const url = `http://localhost:${port}`;
+
+      const blocker = PuppeteerBlocker.parse(`###replace0 > .ad
+localhost:${port}$replace=/.display === 'none'/.display === 'never'/`);
+
+      await blocker.enableBlockingInPage(page);
+      await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+      const value = await page.evaluate(
+        (statusEl) => {
+          if (statusEl === null) {
+            return 'null';
+          }
+
+          return statusEl.textContent;
+        },
+        await page.$('#replace0 > .status'),
+      );
+
+      expect(value).to.be.eql('pass');
+    });
   });
 
   after(async () => {
